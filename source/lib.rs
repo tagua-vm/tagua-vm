a47027529501da00b9efafc9da2edb0d4bc90c3d
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2016-02-19 16:58:02 +0100

    Quality: Format license comment.

--- source/lib.rs
+++ source/lib.rs
@@ -5,10 +5,10 @@
  *
  *
  * New BSD License
- * 
+ *
  * Copyright © 2016-2016, Ivan Enderlin.
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *     * Redistributions of source code must retain the above copyright
@@ -19,7 +19,7 @@
  *     * Neither the name of the Hoa nor the names of its contributors may be
  *       used to endorse or promote products derived from this software without
  *       specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
